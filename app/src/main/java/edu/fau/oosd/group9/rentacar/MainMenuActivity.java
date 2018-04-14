84226a4b8df0bfca67fdb8f46f31f83a6c0c26d6
Marco Pallavicini <mpallavicini2013@fau.edu>
2018-04-14 17:50:34 -0400

    Pretty much finished the main menu and log out function Made lots of progress on the first page of the reservation wizard, but not done yet. Added DatePickerFragment class to handle the date picker for pickup date and dropoff date buttons.

--- app/src/main/java/edu/fau/oosd/group9/rentacar/MainMenuActivity.java
+++ app/src/main/java/edu/fau/oosd/group9/rentacar/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package edu.fau.oosd.group9.rentacar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        //listen for press of Make a Reservation button
+        Button makeReservationButton = (Button) findViewById(R.id.make_a_reservation_button);
+        makeReservationButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if button is pressed, start the reservation activity
+                Intent intent = new Intent(MainMenuActivity.this, ReservOneActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //listen for press of Log Out button
+        Button logOutButton = (Button) findViewById(R.id.log_out_button);
+        logOutButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if button is pressed, start the reservation activity
+                Intent intent = new Intent(MainMenuActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
