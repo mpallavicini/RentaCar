0d7d8865e5d6c14030f7c70e0e0a2a13fe2a28b6
Ohm Trivedi <ohmtrivedi@users.noreply.github.com>
2018-04-14 16:40:23 -0400

    Code Changes 0414181640

---
+++ app/src/main/java/edu/fau/oosd/group9/rentacar/UserProfile.java
@@ -0,0 +1,24 @@
+package edu.fau.oosd.group9.rentacar;
+
+/**
+ * Created by Ohm on 04/14/2018.
+ */
+
+public class UserProfile {
+
+    public String getUserEmailAddress() {
+        return userEmailAddress;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    private String userName;
+    private String userEmailAddress;
+    private String userPassword;
+    private String userPhone;
+    private String userAddress;
+    private String userDL;
+
+}
