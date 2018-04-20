6dfc0ab0f68e76d5edfb5eb8e2e0b3f8016d4e3d
Justin <tino1334@comcast.net>
2018-04-20 15:59:42 -0400

    Reserve activity 2

---
+++ app/src/main/java/edu/fau/oosd/group9/rentacar/ReservTwoActivity.java
@@ -0,0 +1,13 @@
+package edu.fau.oosd.group9.rentacar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserv_two);
+    }
+}
