a84a3b9d6286e8361af8fe7b6777afebfbf10aef
Marco Pallavicini <mpallavicini2013@fau.edu>
2018-04-22 17:30:00 -0400

    Finished reservfiveactivity, just need to store in model.

--- app/src/main/java/edu/fau/oosd/group9/rentacar/ReservFiveActivity.java
+++ app/src/main/java/edu/fau/oosd/group9/rentacar/ReservFiveActivity.java
@@ -1,13 +1,107 @@
 package edu.fau.oosd.group9.rentacar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+/**
+ * The fifth page of the reservation wizard.
+ */
 public class ReservFiveActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText userAddress;
+    EditText userPhone;
+    EditText userLicense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserv_five);
+
+        userName = findViewById(R.id.user_name);
+        userAddress = findViewById(R.id.user_address);
+        userPhone = findViewById(R.id.user_phone);
+        userLicense = findViewById(R.id.user_license);
+
+        //listen for press of BACK button and go to ReservFourActivity
+        Button backButton = (Button) findViewById(R.id.reservfive_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if button is pressed, start the previous activity
+                Intent intent = new Intent(ReservFiveActivity.this, ReservFourActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //listen for press of SUBMIT button and call attemptSubmit()
+        Button submitButton = (Button) findViewById(R.id.reservfive_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptSubmit();
+            }
+        });
+    }
+
+    /**
+     * Attempts submit. Checks for errors (empty fields). If there are errors, they are presented
+     * and no actual submit is made.
+     */
+    private void attemptSubmit() {
+
+        //stores values at the time of submit attempt
+        String name = userName.getText().toString();
+        String address = userAddress.getText().toString();
+        String phone = userPhone.getText().toString();
+        String license = userLicense.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        //check for valid name
+        if (TextUtils.isEmpty(name)) {
+            userName.setError("This field is required");
+            focusView = userName;
+            cancel = true;
+        }
+
+        //check for valid address
+        if (TextUtils.isEmpty(address)) {
+            userAddress.setError("This field is required");
+            focusView = userName;
+            cancel = true;
+        }
+
+        //check for valid phone
+        if (TextUtils.isEmpty(phone)) {
+            userPhone.setError("This field is required");
+            focusView = userName;
+            cancel = true;
+        }
+
+        //check for valid license
+        if (TextUtils.isEmpty(license)) {
+            userLicense.setError("This field is required");
+            focusView = userName;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            //TODO: save the user's entries
+
+            //if successful, start the next activity
+            Intent intent = new Intent(ReservFiveActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
