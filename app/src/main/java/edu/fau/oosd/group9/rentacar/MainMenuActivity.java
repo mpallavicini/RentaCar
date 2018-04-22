3dfd2780a23a8d47f3bc30c113087bb1db5674ec
Marco Pallavicini <mpallavicini2013@fau.edu>
2018-04-22 14:25:04 -0400

    Fixed issues next in reservation wizard.

--- app/src/main/java/edu/fau/oosd/group9/rentacar/MainMenuActivity.java
+++ app/src/main/java/edu/fau/oosd/group9/rentacar/MainMenuActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
